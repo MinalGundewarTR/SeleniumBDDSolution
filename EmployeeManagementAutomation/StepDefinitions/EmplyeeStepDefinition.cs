b0ee6714cbe0f1fc9be47dd67ee09069aae62187
minalGundewarTR <minal.gundewar@thomsonreuters.com>
2024-09-27 11:53:09 +0530

    Scneario Outline with DataTable example

---
+++ EmployeeManagementAutomation/StepDefinitions/EmplyeeStepDefinition.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace EmployeeManagementAutomation.StepDefinitions
+{
+    [Binding]
+    public class EmplyeeStepDefinition
+    {
+        [When(@"I click on PIM menu")]
+        public void WhenIClickOnPIMMenu()
+        {
+            
+        }
+
+        [When(@"I click on Add Employee menu")]
+        public void WhenIClickOnAddEmployeeMenu()
+        {
+            
+        }
+
+        [When(@"I fill the new employee details")]
+        public void WhenIFillTheNewEmployeeDetails(Table table)
+        {
+            
+        }
+
+        [When(@"I click on save")]
+        public void WhenIClickOnSave()
+        {
+            
+        }
+
+        [Then(@"I should see the profile name as '(.*)'")]
+        public void ThenIShouldSeeTheProfileNameAs(string p0)
+        {
+            
+        }
+
+        [Then(@"I should verify the employee full name in the text field")]
+        public void ThenIShouldVerifyTheEmployeeFullNameInTheTextField()
+        {
+            
+        }
+    }
+}
